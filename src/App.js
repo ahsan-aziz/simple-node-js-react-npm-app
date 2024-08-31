f7b272a818a1078d3112e7609b5074d1d43376ad
ahsan-aziz <mahsanaziz@gmail.com>
2024-08-31 21:53:42 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">TMLS</h1>
+          <h1 className="App-title">BASHIR</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
