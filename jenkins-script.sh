38f79a5cf8f4dcefef2175a8b96260bff642ceed
ahsan-aziz <mahsanaziz@gmail.com>
2024-08-31 22:38:20 +1000

    Update jenkins-script.sh

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -1,2 +1,2 @@
 scp -P 2222 -i ~/.ssh/id_rsa -r ./ root@10.0.2.15:./
-ssh -i ~/.ssh/id_rsa -f root@10.0.2.15 -p 2222 "chmod +x ./node-script.sh; ./node-script.sh; exit" 
+ssh -i ~/.ssh/id_rsa -f root@10.0.2.15 -p 2222 "chmod +x ./node-script.sh; ./node-script.sh; sleep 5; exit" 
